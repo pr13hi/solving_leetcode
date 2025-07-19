76023bf791947ec5a4b619806336ce6c93f02c0a
Prachi <prachishaw04@gmail.com>
2025-07-19 23:24:42 +0530

    Update Question 2.py

--- Question 2.py
+++ Question 2.py
@@ -1,6 +1,13 @@
 # Concept name : Array / String
 
 # Question : Greatest Common Divisor of Strings
+            # For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+            # Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+            
+            #                 Example 1:
+                            
+            #                 Input: str1 = "ABCABC", str2 = "ABC"
+            #                 Output: "ABC"
 
 # Solution :
 
@@ -15,10 +22,10 @@ class Solution(object):
         return self.gcdOfStrings(str1, str2[len(str1):])
 
 # Explanation : First check if str1 + str2 is equal to str2 + str1. If not, return an empty string because that means there's no common pattern that can build both strings. 
-# If the condition passes, apply a recursive strategy similar to the Euclidean algorithm used for numbers. If the lengths are equal, return either string as the result.
-# If one string is longer, remove the matching prefix (of the shorter string’s length) from the longer string and call the function again.
-# This way, gradually reduce the problem until we reach the base case where both strings are equal—which is the greatest common divisor string.
-
-# [Euclidean algorithm -- The Euclidean algorithm is a method to find the greatest common divisor (GCD) of two numbers.
-# It works by repeatedly replacing the larger number with the remainder when the larger is divided by the smaller.
-# This continues until the remainder becomes zero — and at that point, the other number is the GCD.]
+                # If the condition passes, apply a recursive strategy similar to the Euclidean algorithm used for numbers. If the lengths are equal, return either string as the result.
+                # If one string is longer, remove the matching prefix (of the shorter string’s length) from the longer string and call the function again.
+                # This way, gradually reduce the problem until we reach the base case where both strings are equal—which is the greatest common divisor string.
+                
+                # [Euclidean algorithm -- The Euclidean algorithm is a method to find the greatest common divisor (GCD) of two numbers.
+                # It works by repeatedly replacing the larger number with the remainder when the larger is divided by the smaller.
+                # This continues until the remainder becomes zero — and at that point, the other number is the GCD.]
