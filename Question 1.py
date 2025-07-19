26038291f6158cae121ba86f736a97e549364f76
Prachi <prachishaw04@gmail.com>
2025-07-19 23:22:45 +0530

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,6 +1,18 @@
 # Concept name : Array / String
 
 # Question : Merge Strings Alternately
+            # You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. 
+            # If a string is longer than the other, append the additional letters onto the end of the merged string.
+            # Return the merged string.
+        
+            #             Example 1:
+                        
+            #             Input: word1 = "abc", word2 = "pqr"
+            #             Output: "apbqcr"
+            #             Explanation: The merged string will be merged as so:
+            #             word1:  a   b   c
+            #             word2:    p   q   r
+            #             merged: a p b q c r
 
 # Solution : 
 
@@ -16,5 +28,5 @@ class Solution(object):
         return word
 
 # Explanation : Determine the longer length between the two strings to ensure the loop covers all characters. 
-# Then, using a for loop, iterate through that length and add characters alternately from both word1 and word2, 
-# but only if the current index exists in that string. This way, we can avoid index errors and successfully merge both strings character by character.
+                # Then, using a for loop, iterate through that length and add characters alternately from both word1 and word2, 
+                # but only if the current index exists in that string. This way, we can avoid index errors and successfully merge both strings character by character.
